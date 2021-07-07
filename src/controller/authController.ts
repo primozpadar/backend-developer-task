b80f4ecbd9878fa635601427fc2bfd279cb67601
primozpadar <primoz.padar1@gmail.com>
2021-07-07 12:41:31 +0200

    auth controller comments

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -4,6 +4,11 @@ import jwt from 'jsonwebtoken';
 import { User } from '../entity/User';
 import { ApiError } from '../handlers/error';
 
+/**
+ * USER REGISTER
+ * It creates new user in database (password is hashed).
+ * Username must be unique. If it's not, it returns error with code 403.
+ */
 export const register = async (req: Request, res: Response, next: NextFunction) => {
   const { name, username, password } = req.body;
 
@@ -21,6 +26,12 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
   return res.json({ status: 'success' });
 };
 
+/**
+ * USER LOGIN
+ * This handler finds user and verifies his password.
+ * If everything is correct, it creates JWT authentication token and sends it back.
+ * Token expires after 24h.
+ */
 export const login = async (req: Request, res: Response, next: NextFunction) => {
   const { username, password } = req.body;
 
@@ -39,6 +50,10 @@ export const login = async (req: Request, res: Response, next: NextFunction) => 
   res.json({ status: 'success', authToken });
 };
 
+/**
+ * CHANGE PASSWORD
+ * Changes user's password
+ */
 export const changePassword = async (req: Request, res: Response, next: NextFunction) => {
   const { username, oldPassword, newPassword } = req.body;
 
@@ -58,11 +73,21 @@ export const changePassword = async (req: Request, res: Response, next: NextFunc
   return res.json({ status: 'success' });
 };
 
+/**
+ * Function extracts token from request headers.
+ * @returns JWT Token or undefined
+ */
 function getToken(req: Request) {
   const authHeader = req.headers.authorization;
   return authHeader?.split(' ')[1];
 }
 
+/**
+ * AUTHENTICATE USER
+ * Middleware that authenticates user.
+ * JWT token is decoded and data id added to request (req.user)
+ * If token is missing, user gets back an error.
+ */
 export const authenticateUser = async (req: Request, _res: Response, next: NextFunction) => {
   const token = getToken(req);
   if (!token) return next(new ApiError(401, 'token missing'));
@@ -75,6 +100,12 @@ export const authenticateUser = async (req: Request, _res: Response, next: NextF
   });
 };
 
+/**
+ * AUTHENTICATE USER WITHOUT ERROR
+ * The same middleware as 'authenticate user' but no error
+ * is thrown in case of missing token.
+ * This middleware is used for example for public/private notes
+ */
 export const authenticateUserWithoutError = async (req: Request, _res: Response, next: NextFunction) => {
   const token = getToken(req);
   if (!token) return next();
